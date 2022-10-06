492b87ef8e93566b57fae68c715c5fd74fb40427
Albert R. Burnyashov <albrburn@gmail.com>
2022-10-06 22:50:50 +0300

    Added animation

--- app/src/main/java/com/droiddev26/pluto_ev/MainActivity.kt
+++ app/src/main/java/com/droiddev26/pluto_ev/MainActivity.kt
@@ -7,10 +7,21 @@ import android.widget.ImageView
 
 var click = 0
 
+const val scaleMinus = 0f
+const val scalePlus = 1f
+const val duration = 1500L
+const val rotation = 1080f
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pluto1ImageView = findViewById<ImageView>(R.id.pluto_1)
+        pluto1ImageView.animate().rotation(rotation).scaleX(scalePlus).scaleY(
+            scalePlus).setDuration(duration)
 
     }
 
@@ -19,60 +30,76 @@ class MainActivity : AppCompatActivity() {
 
         when (click) {
             1 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_1)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_1)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_2)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_2)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             2 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_2)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_2)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_3)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_3)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             3 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_3)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_3)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_4)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_4)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             4 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_4)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_4)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_5)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_5)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             5 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_5)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_5)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_6)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_6)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             6 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_6)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_6)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_7)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_7)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             7 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_7)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_7)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_my)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_my)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
             8 -> {
-                var pluto1ImageView = findViewById<ImageView>(R.id.pluto_my)
-                pluto1ImageView.animate().alpha(0.0F).setDuration(2000)
+                val pluto1ImageView = findViewById<ImageView>(R.id.pluto_my)
+                pluto1ImageView.animate().alpha(0.0F).rotation(-rotation).scaleX(scaleMinus).scaleY(
+                    scaleMinus).setDuration(duration)
 
-                var pluto2ImageView = findViewById<ImageView>(R.id.pluto_1)
-                pluto2ImageView.animate().alpha(1.0F).setDuration(2000)
+                val pluto2ImageView = findViewById<ImageView>(R.id.pluto_1)
+                pluto2ImageView.animate().alpha(1.0F).rotation(rotation).scaleX(scalePlus).scaleY(
+                    scalePlus).setDuration(duration)
             }
         }
 
